0579f5b73a48e5071d7ebeef5b89fdafb17379f5
David Johnston <david@blacksheepcode.com>
2018-07-07 19:27:54 +1000

    attempt at gh page

---
+++ docs/App.tsx
@@ -0,0 +1,46 @@
+
+import * as React from 'react';
+import { IColor, RgbaPicker } from "../src/RgbaPicker";
+
+interface IAppState {
+  color: IColor;
+}
+
+class App extends React.Component<any, IAppState> {
+
+  public state: IAppState;
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      color: null
+    };
+  }
+
+  public onChange = (c: IColor) => {
+    this.setState({
+      color: c
+    });
+  }
+
+  public render() {
+
+    return (
+      <div className="App">
+        <RgbaPicker color={{
+          a: 1,
+          b: 50,
+          g: 111,
+          r: 11
+       }}
+       onChange = {
+         this.onChange
+       } />
+
+        {JSON.stringify(this.state.color)}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack-docs.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+    entry: "./docs/App.tsx",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/docs", 
+        library: "hello",
+        libraryTarget: 'umd'
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            disable: false, // webpack@2.x and newer
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+              }
+        ]
+    },
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    // externals: {
+    //     "react": "React",
+    //     "react-dom": "ReactDOM"
+    // }
+};
